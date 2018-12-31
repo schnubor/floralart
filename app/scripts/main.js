d4642462fb4b49361b03552c01c585a103e965f5
schnubor <info@chko.org>
2018-12-31 02:23:25 +0100

    gallery updates

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,44 @@
-var updateNavbar = function ( scrollTop ) {
+var updateNavbar = function (scrollTop) {
     var opacity = scrollTop / $(window).height() * 0.6;
 
-    $('.navbar').css( 'backgroundColor', 'rgba(0,0,0, ' + opacity + ')' );
+    $('.navbar').css('backgroundColor', 'rgba(0,0,0, ' + opacity + ')');
 }
 
 var onWindowScroll = function (params) {
     var scrollTop = $(window).scrollTop();
 
-    if( scrollTop <= $(window).height() ) {
-        updateNavbar( scrollTop );
+    if (scrollTop <= $(window).height()) {
+        updateNavbar(scrollTop);
     }
 }
 
-window.addEventListener( 'scroll', onWindowScroll );
+window.addEventListener('scroll', onWindowScroll);
+
+lightbox.option({ wrapAround: true })
 
 
+// Select all links with hashes
+$('a[href*="#"]')
+    // Remove links that don't actually link to anything
+    .not('[href="#"]')
+    .not('[href="#0"]')
+    .click(function (event) {
+        // On-page links
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+            &&
+            location.hostname == this.hostname
+        ) {
+            // Figure out element to scroll to
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            // Does a scroll target exist?
+            if (target.length) {
+                // Only prevent default if animation is actually gonna happen
+                event.preventDefault();
+                $('html, body').animate({
+                    scrollTop: target.offset().top - 80
+                }, 1000 );
+            }
+        }
+    });
