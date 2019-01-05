087ea1012a11e9779d09cf31acc2863e7342a8ad
schnubor <info@chko.org>
2019-01-05 21:14:11 +0100

    final

--- gulpfile.js
+++ gulpfile.js
@@ -166,7 +166,7 @@ gulp.task('wiredep', () => {
     .pipe(gulp.dest('app'));
 });
 
-gulp.task('build', ['lint', 'html', 'images', 'fonts', 'extras'], () => {
+gulp.task('build', ['lint', 'html', 'images', 'fonts', 'extras', 'styles', 'scripts'], () => {
   return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
 });
 
