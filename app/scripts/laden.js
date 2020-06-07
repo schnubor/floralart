4342ff6b7c88ee44b33733fad6565183a721a462
Christian Korndoerfer <schnuppser@gmail.com>
2020-06-07 14:35:22 +0200

    oeffnungszeiten

--- app/scripts/laden.js
+++ app/scripts/laden.js
@@ -1,27 +1,35 @@
+const markdownConverter = new showdown.Converter()
+
 const initCarousel = (name, entryId) => {
-    contentfulClient
-        .getEntry(entryId)
-        .then((entry) => {
-            const urls = extractImageUrls(entry.fields.gallerie);
-            const elements = urls.map(url => {
-                return `<div><img src="${url}" alt="Laden"></div>`;
-            });
-            elements.join();
-            $(`.carousel.${name}`).append(elements);
-            initSlick();
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+  contentfulClient
+    .getEntry(entryId)
+    .then((entry) => {
+      const urls = extractImageUrls(entry.fields.gallerie)
+      const elements = urls.map(url => {
+        return `<div><img src="${url}" alt="Laden"></div>`
+      })
+      elements.join()
+      $(`.carousel.${name}`).append(elements)
+      initSlick()
+    })
+    .catch((error) => {
+      console.log(error)
+    })
 }
 
-initCarousel('laden', '3bDAKaouakEokESCWCuIQk');
+initCarousel('laden', '3bDAKaouakEokESCWCuIQk')
 
 contentfulClient
-    .getEntry('3bDAKaouakEokESCWCuIQk')
-    .then((entry) => {
-        console.log(entry);
-        $('.title.laden').text(entry.fields.headline);
-        $('.subtitle.laden').text(entry.fields.description);
-    });
+  .getEntry('3bDAKaouakEokESCWCuIQk')
+  .then((entry) => {
+    $('.title.laden').text(entry.fields.headline)
+    $('.subtitle.laden').text(entry.fields.description)
+  })
+
+contentfulClient
+  .getEntry('1seBFV6TlPELg7pcsBP4lG')
+  .then((entry) => {
+    $('.title.copenhourse').text(entry.fields.title)
+    $('.subtitle.copenhourse').html(markdownConverter.makeHtml(entry.fields.text))
+  })
 
